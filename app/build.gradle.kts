3170544a1d5f5f832e873aa2a1476e9533c93ec5
NehaK <neha.kushwah993@gmail.com>
2026-02-13 16:46:35 -0800

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2026.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -71,7 +71,7 @@ dependencies {
     implementation("androidx.media3:media3-ui:1.4.0")
 
     // parse json
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("com.google.code.gson:gson:2.13.2")
 
     //coil
     implementation("io.coil-kt:coil-compose:2.7.0") 
